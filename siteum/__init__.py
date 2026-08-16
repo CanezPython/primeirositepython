e722b9c5bb1efd2c208f047d83e1304490e7d3b1
CanezPython <canezpython@gmail.com>
2026-08-15 22:35:03 -0300

    Refactor database URI configuration
    Update database configuration to use environment variable if available.

--- siteum/__init__.py
+++ siteum/__init__.py
@@ -2,12 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site1.db"
 app.config['SECRET_KEY'] = 'e81c1a3801e816e13b7768fd67a64b4f'
+if os.getenv("DATABASE_URL"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site1.db"
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt (app)
