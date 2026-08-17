bba3fad5cecaa6439d3216fa2f2d0a873b20e8c8
CanezPython <canezpython@gmail.com>
2026-08-16 22:32:32 -0300

    Initialize SQLAlchemy engine and check for 'usuario' table
    Added SQLAlchemy engine and table creation logic.

--- siteum/__init__.py
+++ siteum/__init__.py
@@ -3,11 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+import sqlalchemy
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'e81c1a3801e816e13b7768fd67a64b4f'
-
 if os.getenv("DATABASE_URL"):
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 else:
@@ -18,6 +18,18 @@ bcrypt = Bcrypt (app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
+
+
+from siteum import models
+engine = sqlalchemy.create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table("usuario"):
+    with app.app_conext():
+        database.drop_all()
+        database.create_all()
+        print("Base de Dados Criada com Sucesso")
+else:
+    print("Base de Dados ja Existente")
 
 
 from siteum import routes
