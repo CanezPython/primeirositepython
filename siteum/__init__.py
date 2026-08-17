bb3296066826abc1adfb6ac5c8e3e0b41579a3d1
CanezPython <canezpython@gmail.com>
2026-08-16 23:10:47 -0300

    Fix syntax errors and improve database initialization

--- siteum/__init__.py
+++ siteum/__init__.py
@@ -9,9 +9,9 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'e81c1a3801e816e13b7768fd67a64b4f'
 if os.getenv("DATABASE_URL"):
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 else:
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site1.db"
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site1.db'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt (app)
@@ -21,11 +21,10 @@ login_manager.login_message_category = 'alert-info'
 
 
 from siteum import models
-engine = sqlalchemy.create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 inspector = sqlalchemy.inspect(engine)
-
 if not inspector.has_table("usuario"):
-    with app.app_conext():
+    with app.app_context():
         database.drop_all()
         database.create_all()
         print("Base de Dados Criada com Sucesso")
